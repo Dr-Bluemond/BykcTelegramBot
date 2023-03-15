87338b121e4f9f2b2529c1e60d0e2a820d5fbf6d
Bluemond <dr_bluemond@126.com>
2023-03-15 23:19:36 +0800

    little

--- src/main.py
+++ src/main.py
@@ -366,7 +366,7 @@ async def refresh_course_list(context: ContextTypes.DEFAULT_TYPE):
         course_data.selected = course['selected']
         course_data.sync_model()
         if course_data.is_select_start_date_changed():
-            __add_rush_job(context.job_queue, course.id,
+            __add_rush_job(context.job_queue, course_data.id,
                            datetime.datetime.strptime(course_data.select_start_date, '%Y-%m-%d %H:%M:%S'))
         if not course_data.is_notified():
             message = course_data.get_info(is_detail="no", title='【新的博雅】')
